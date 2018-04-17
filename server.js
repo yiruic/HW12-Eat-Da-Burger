c6890fd12a3d95455597f8accdba8b8cfd8d9e66
Yirui Chen <yiruic@users.noreply.github.com>
2018-04-16 19:21:46 -0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var db = require('./db');
+
+var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+
+// listen on port 3000
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+
+
+console.log(module.exports);
