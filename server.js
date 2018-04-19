449664578a47001147b13bf66c52857a1f929281
Yirui Chen <yiruic@users.noreply.github.com>
2018-04-18 19:42:19 -0700

    m

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require('body-parser');
-var db = require('./models');
+var db = require('./models/');
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
