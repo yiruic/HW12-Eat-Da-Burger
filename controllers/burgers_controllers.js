422190661b14fa60b915a5bb8cca5f221bbf0c2d
Yirui Chen <yiruic@users.noreply.github.com>
2018-04-18 19:25:41 -0700

    update

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var burger = require('../models/burger.js')
+var burger = require('../models/burger.js');
 
 //Create the `router` for the app, and export the `router` at the end of your file.
 var express = require("express");
